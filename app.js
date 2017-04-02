9c8248965d432ea8aff34fa8b3a5b1b2a90e8eb2
Mike Hasko <psychopez@gmail.com>
2017-04-02 13:06:00 -0400

    Add more commenting, add .gitignore file

--- app.js
+++ app.js
@@ -35,52 +35,64 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
-//Router will set up
-var router = express.Router();              // get an instance of the express Router
+//Router will set up the api calls.  It reads in all the http traffic and 'routes'
+//  it to specific endpoints
+var router = express.Router();  // get an instance of the express Router
 
-//REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+//All of our routes will be prefixed with /api.  Since the front end stuff is in
+//  /dist and thus at root, it can follow its own heiarchry, while everything with
+//  /api will follow the heiarchry we define in router
 app.use('/api', router);
 
+//every router bounce call will go through here.  We can use this to set up basic
+//  logging or configuration of headers or what ever needs to be done on all api
+//  calls.
 router.use(function(req, res, next) {
-    // do logging
+    //TODO - using console.log is usually not good, find a logging library?
     console.log('Something is happening.');
     next();
 });
 
-// test route to make sure everything is working
+/* GET /api/ping */
+//Our first API call, will simply return a silly little string.  Good for smoke
+//  testing if the server is running correctly or not.
 router.route('/ping')
     .get(function(req, res) {
         res.json({message: 'Ping! I am alive'});
     });
 
+/* GET /api/summoner/:sumname */
+//Pass the entered summoner name and determine the summonerid using the Riot API.
+//  Once we have the id, use the summoner id to get its match history
 router.route('/summoner/:sumname')
     .get(function(req, res) {
         // TODO - check to see if this user is stored in our local db, if not fetch
         LolApi.Summoner.getByName(req.params.sumname, function(err, summoner) {
             if (!err) {
-                //res.json(summoner[req.params.sumname].id);
 
                 // TODO - check to see if match history is < x hours old, if so, fetch new history
-                //      - and save to local db
+                // TODO - and save to local db
                 //
 
                 LolApi.getMatchHistory(summoner[req.params.sumname].id, null, 'na', function(err, history){
                     if(!err){
+                        //TODO - we need to only save the new match history data in local db
+
+                        //Later on we'll want to do something more exciting with the match history, but for
+                        //  now, just return the json of the history
                         res.json(history);
                     }
                 });
-
-
-
             }
         });
     });
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
+    //TODO - using console.log is usually not good, find a logging library?
     console.log("ERROR: " + reason);
     res.status(code || 500).json({"error": message});
 }
 
+//Boilerplate node.js thing to make all our code visible and usable
 module.exports = app;
--- dist/js/groupedGames.js
+++ dist/js/groupedGames.js
@@ -1,17 +1,54 @@
 (function () {
     'use strict';
 
+    //This is angular boilerplate for creating an angular module.  This module's name
+    //  is groupedGames.  We can include other angular modules by listing them inside the
+    //  square brackets.  Since we're not including any other modules right now, the brackets
+    //  are empty.
+    //
+    //  We then create the groupedGameCtrl controller.  The name of any given controller
+    //  is the first parameter of the .controlller() function, and its defination is the
+    //  second parameter.  A lot of other code you'll see online has an inline anonomus
+    //  function (ex .controller(nameCtrl, function(){ -- lots of code });), but I like to
+    //  create a named function and just add the name of that funciton in this line.  It's
+    //  a few more lines of code, but it is cleaner and easier to debug (This matches with
+    //  John Papa's angular style guide https://github.com/johnpapa/angular-styleguide/tree/master/a1
     angular
         .module('groupedGames',[])
         .controller('groupedGameCtrl', GroupedGameCtrl);
 
 
+    //Angular's secret sauce is injecting things when they are needed.  Again, in a lot of
+    //  online code you'll see injection different ex:
+    // .controller('nameCtrl',
+    //  ['$location', '$routeParams', 'common', 'dataservice',
+    //    function Dashboard($location, $routeParams, common, dataservice) {}
+    //  ]);
+    // but that's hard to read and doesn't explictly call out what's going on.  Using the
+    // $inject service on a function then having the function definiation with the injected
+    // services as parrams into the controller function helps spell out what's going on and
+    // makes the code cleaner without having additional private anon functions everywhere.
     GroupedGameCtrl.$inject = ['$scope', '$log', '$http'];
     function GroupedGameCtrl($scope, $log, $http){
+        //summoner1Results is now a variable on the controller scope.  This allows the template
+        //  to access summoner1Results
         $scope.summoner1Results = "summoner data will appear here";
 
         $scope.getSummoner = function(){
+            //Likewise, summonerName1 was defined in scope in the template, and now we can
+            //  accesss what ever the value is here.  We pull the string value from the input
+            //  field, and add it to this url string...
             var url = '/api/summoner/'+ $scope.summonerName1;
+            //...Then we use the url string to make the call to our API.  $http is an angular
+            //  service that makes http calls, given a few parameters like the type of request
+            //  in this case a GET, and url, in this case the url we created.  When we get a
+            //  successful response back, we'll execute the code in the successCallback function,
+            //  otherise if we get an error response we'll execute teh code in the errorCallback
+            //  function.
+            //
+            //  Either way, we set the summoner1Results variable to either the response, or the
+            //  string 'err'.  As this variable is bound to the template, we easily display the
+            //  result to the user.
             $http({
                 method: 'GET',
                 url: url
